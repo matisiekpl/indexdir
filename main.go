2a5c7a71af3a0b289c9e6f1f89e75098356c6a76
Mateusz Woźniak <matisiek11@gmail.com>
2023-08-28 16:28:08 +0200

    Added recursive mode

--- main.go
+++ main.go
@@ -4,31 +4,43 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	wd, err := os.Getwd()
+	filepath.WalkDir(".", walk)
+}
+
+func walk(s string, d fs.DirEntry, err error) error {
 	if err != nil {
-		logrus.Panic(errors.New("failed to get working directory"))
+		return err
 	}
+	if d.IsDir() {
+		generate(s)
+	}
+	return nil
+}
+
+func generate(wd string) {
 	logrus.Info("working directory: ", wd)
-	entries, err := os.ReadDir("./")
+	entries, err := os.ReadDir(wd)
 	if err != nil {
 		logrus.Panic(errors.New("failed to read directory"))
 	}
 
 	buf := bytes.NewBufferString("")
 	for _, entry := range entries {
-		if strings.HasPrefix(entry.Name(), ".") {
+		if strings.HasPrefix(entry.Name(), ".") || entry.Name() == "index.html" {
 			continue
 		}
-		buf.WriteString(fmt.Sprintf("<a href='%s'>%s</a><br/>\n", entry.Name(), entry.Name()))
+		buf.WriteString(fmt.Sprintf("<a href='/%s/%s'>%s</a><br/>\n", wd, entry.Name(), entry.Name()))
 	}
-	err = os.WriteFile("index.html", buf.Bytes(), 0644)
+	err = os.WriteFile(wd+"/index.html", buf.Bytes(), 0644)
 	if err != nil {
 		logrus.Panic(errors.New("failed to write file"))
 	}
